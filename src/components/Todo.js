151a966332b4b9eb8b036b29ca5e199cd6b25e9c
Minh <minhnhatdang1111@gmail.com>
2021-01-17 22:32:24 -0500

    Changed app name

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,53 +1,66 @@
-import React, {useState, useEffect} from 'react'
-import {RiCloseCircleLine} from 'react-icons/ri';
-import {TiEdit} from 'react-icons/ti';
-import TodoForm from './TodoForm';
+import React, { useState, useEffect } from "react";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { TiEdit } from "react-icons/ti";
+import TodoForm from "./TodoForm";
 
-function Todo({todos, completeTodo, deleteTodo, updateTodo}) {
+function Todo({ todos, completeTodo, deleteTodo, updateTodo }) {
+  const [editTodo, setEditTodo] = useState({
+    id: null,
+    value: "",
+    edit: false,
+    completed: null,
+  });
 
-    const [editTodo, setEditTodo] = useState({
-        id: null,
-        value: '',
-        edit: false,
-        completed: null
-    })
+  const handleUpdate = () => {
+    setEditTodo({
+      id: null,
+      value: "",
+      edit: false,
+      completed: null,
+    });
+  };
 
-<<<<<<< HEAD
+  return (
+    <div className="space-y-3 space-x-3 flex flex-wrap justify-center bg-green-100 px-4 py-2 m-2">
+      {todos.map((todo, index) =>
+        editTodo.id === todo.id ? (
+          <TodoForm
+            key={index}
+            edit={editTodo}
+            updateTodo={updateTodo}
+            handleUpdate={handleUpdate}
+          />
+        ) : (
+          <div
+            key={index}
+            className={`border-2 border-dashed cursor-pointer flex items-center p-1 rounded ${
+              todo.completed
+                ? "line-through text-gray-500 italic border-gray-300"
+                : "border-green-300"
+            }`}
+          >
+            <div className="mr-2" onClick={() => completeTodo(todo.id)}>
+              {todo.value}
+            </div>
+            <RiCloseCircleLine
+              className=""
+              onClick={() => deleteTodo(todo.id)}
+            />
+            <TiEdit className="" onClick={() => setEditTodo(todo)} />
+          </div>
+        )
+      )}
+    </div>
+  );
 
-=======
->>>>>>> db8a26156e1e78d71989163987be7b567756b05a
-
-    const handleUpdate = () => {
-        setEditTodo({
-            id: null,
-            value: '',
-            edit: false,
-            completed: null
-        })
-    }
-
-    return(
-        <div className="space-y-3 space-x-3 flex flex-wrap justify-center bg-green-100 px-4 py-2 m-2">
-            {todos.map((todo, index) =>
-                (editTodo.id === todo.id) ? 
-                (<TodoForm key={index} edit={editTodo} updateTodo={updateTodo} handleUpdate={handleUpdate}/>) :
-                (<div key={index} className={`border-2 border-dashed cursor-pointer flex items-center p-1 rounded ${todo.completed ? 'line-through text-gray-500 italic border-gray-300': 'border-green-300'}`}>
-                    <div className="mr-2" onClick={() => completeTodo(todo.id)}>{todo.value}</div>
-                    <RiCloseCircleLine className="" onClick={() =>deleteTodo(todo.id)}/>
-                    <TiEdit className="" onClick={() => setEditTodo(todo)}/>
-                </div>)
-             )}
-        </div>
-    ) 
-    
-    // (editTodo.id === todo.id) ?
-    //  (<TodoForm key={index} edit={editTodo} updateTodo={updateTodo} handleUpdate={handleUpdate}/>): (
-        // <div key={index} className={todo.completed ? 'todo-row complete': 'todo-row'}>
-        //     <div className="task flex-item" onClick={() => completeTodo(todo.id)}>{todo.value}</div>
-        //     <RiCloseCircleLine className="delete-btn" onClick={() =>deleteTodo(todo.id)}/>
-        //     <TiEdit className="edit-btn" onClick={() => setEditTodo(todo)}/>
-        // </div>
-    // ) 
+  // (editTodo.id === todo.id) ?
+  //  (<TodoForm key={index} edit={editTodo} updateTodo={updateTodo} handleUpdate={handleUpdate}/>): (
+  // <div key={index} className={todo.completed ? 'todo-row complete': 'todo-row'}>
+  //     <div className="task flex-item" onClick={() => completeTodo(todo.id)}>{todo.value}</div>
+  //     <RiCloseCircleLine className="delete-btn" onClick={() =>deleteTodo(todo.id)}/>
+  //     <TiEdit className="edit-btn" onClick={() => setEditTodo(todo)}/>
+  // </div>
+  // )
 }
 
-export default Todo
+export default Todo;
